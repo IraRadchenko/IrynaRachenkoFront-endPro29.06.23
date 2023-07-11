20b80e5de5c222c7ea49e6f26a6dda52ecbf8959
IraRadcenko <goncharovaira@ukr.net>
2023-07-11 15:36:31 +0300

    HW3

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let name=prompt('What is your name?','');
-alert(`Hello, ${name}! How are you?`);
+"use strict"
+
+//завдання 1
+
+let firstName = prompt('Ваше ім`я?');
+let lastName = prompt('Ваше прізвище?');
+let city = prompt('Ваше місто?');
+alert('Вітаю, '+firstName+' '+lastName+'! Ваше місто '+city+'.');
+
+//завдання 2
+
+let name = prompt('Ваше ім`я?');
+let age = prompt('Ваш вік?');
+alert(`Вітаю, ${name}! \nВаш вік ${age}.`);
