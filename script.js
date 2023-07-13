89fefa02ed5b90d2a054e257f516f3b0596155bd
IraRadcenko <goncharovaira@ukr.net>
2023-07-13 14:27:48 +0300

    HW5

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict"
 
-let hours = prompt('Будь ласка, введіть кількість годин.');
-let seconds = hours * 3600;
-alert(seconds);
+let firstNumber = Number(prompt('Будь ласка, введіть перше число.'));
+let secondNumber = Number(prompt('Будь ласка, введіть друге число.'));
+let thirdNumber = Number(prompt('Будь ласка, введіть третє число.'));
+let arithmeticMean = (firstNumber + secondNumber + thirdNumber) / 3;
+alert(arithmeticMean);
