ca6de1cfd76ab69f504852c9f0154d7cd3b46686
IraRadcenko <goncharovaira@ukr.net>
2023-07-13 14:24:15 +0300

    HW4

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 "use strict"
 
-//завдання 1
-
-let firstName = prompt('Ваше ім`я?');
-let lastName = prompt('Ваше прізвище?');
-let city = prompt('Ваше місто?');
-alert('Вітаю, ' + firstName + ' ' + lastName + '! Ваше місто ' + city + '.');
-
-//завдання 2
-
-let name = prompt('Ваше ім`я?');
-let age = prompt('Ваш вік?');
-alert(`Вітаю, ${name}! 
-Ваш вік ${age}.`);
+let hours = prompt('Будь ласка, введіть кількість годин.');
+let seconds = hours * 3600;
+alert(seconds);
