cba9906ddb285d48cfee00c26c97559137567bae
IraRadcenko <goncharovaira@ukr.net>
2023-07-31 18:07:49 +0300

    HW12

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict"
+/*Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+    Наприклад:
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5 );
+console.log(array);
+  Результат: [1, 2, 3, 4, 6, 7]
+*/
+const arr = [1, 5, 9, 8, 3];
+const numberToDelete = 9;
+function removeElement(array, item) {
+    return array.filter((el) => el !== item);
+}
+
+console.log(removeElement(arr, numberToDelete));
